9db6c42afcfc96d85e2e2affee50c5edfc2ca909
Tony <Wu@Tonys-Lob-MacBook.local>
2017-05-22 10:52:36 -0700

    fix(movies): fix GET /movies based on robin's feedback
    fix(movies): add db:seed to travis

--- test/plugins/features/movies/index.test.js
+++ test/plugins/features/movies/index.test.js
@@ -1,10 +1,21 @@
 'use strict';
 
-const Movies = require('../../../../lib/server');
+const Knex         = require('../../../../lib/libraries/knex');
+const Movies       = require('../../../../lib/server');
+const MovieFactory = require('../../../factories/movie');
+
+const movie1 = MovieFactory.build({ name: 'Star Trek', release_year: 1982 });
+const movie2 = MovieFactory.build({ name: 'Star Trek 2', release_year: 1984 });
+const movie3 = MovieFactory.build({ name: 'Star Trok', release_year: 1986 });
 
 describe('movies integration', () => {
 
   describe('create', () => {
+
+    beforeEach(() => {
+      return Knex.raw('TRUNCATE movies CASCADE')
+      .then(() => Knex('movies').insert([movie1, movie2, movie3]));
+    });
 
     it('creates a movie', () => {
       return Movies.inject({
@@ -18,14 +29,19 @@ describe('movies integration', () => {
       });
     });
 
-    it('gets movies prior to a release_year', () => {
+  });
+
+  describe('list', () => {
+
+    it('gets movies after year_start (inclusive)', () => {
       return Movies.inject({
-        url: '/movies?release_year=1980',
+        url: '/movies?year_start=1980',
         method: 'GET'
       })
       .then((response) => {
         expect(response.statusCode).to.eql(200);
-        expect(response.result).not.to.be.null;
+        response.result.map((movie) => expect(movie.object).to.eql('movie'));
+        expect(response.result).to.be.length(3);
       });
     });
 
